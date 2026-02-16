62f89dc82b9a4948bc4bd97cb484902b224d6ae7
Miantra007 <rakotoniainami5@gmail.com>
2026-02-16 16:40:03 +0300

    don

---
+++ script1.sql
@@ -0,0 +1,43 @@
+create DATABASE sinistre if not EXISTS;
+USE sinistre;
+
+CREATE TABLE ville (
+    id_ville INT AUTO_INCREMENT PRIMARY KEY,
+    nom_ville VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE categorie (
+    id_categorie INT AUTO_INCREMENT PRIMARY KEY,
+    nom_categorie VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE produit (
+    id_produit INT AUTO_INCREMENT PRIMARY KEY,
+    id_categorie INT NOT NULL,
+    prix_unitaire DECIMAL(10,2) NOT NULL,
+    nom_produit VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE besoin (
+    id_besoin INT AUTO_INCREMENT PRIMARY KEY,
+    id_ville INT NOT NULL,
+    id_produit INT NOT NULL,
+    quantite INT NOT NULL,
+    date_saisie DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE don (
+    id_don INT AUTO_INCREMENT PRIMARY KEY,
+    id_produit INT NOT NULL,
+    id_ville INT NOT NULL,
+    quantite INT NOT NULL,
+    quantite_Initial INT NOT NULL,
+    date_don DATETIME DEFAULT CURRENT_TIMESTAMP
+
+);
+CREATE TABLE dispatch (
+    id_dispatch INT AUTO_INCREMENT PRIMARY KEY,
+    id_don INT NOT NULL,
+    id_besoin INT NOT NULL,
+    quantite_attribuee INT NOT NULL,
+    date_dispatch DATETIME DEFAULT CURRENT_TIMESTAMP
+);
--- toDo.md
+++ toDo.md
@@ -5,7 +5,7 @@
             -categorie(id_categorie,nom_categorie)
             -produit(id_produit,id_categorie,prix_unitaire,nom_produit)
             -besoin(id_besoin,id_ville,id_produit,quantite_besoin,date_saisie)
-            -don(id_don,id_ville,id_produit,quantite_don)
+            -don(id_don,id_ville,id_produit,quantite_don,quantite_Initial)
             -dispatch(id_dispatch,id_don,id_besoin,quantite_attribuee,date_dispatch)
 
         -inserer donnee de test (Miantra)
