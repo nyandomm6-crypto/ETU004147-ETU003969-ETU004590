e315d3212a5be00775c6e954ac19aa160404fc79
nyandomm4 <nyandomm4@gmail.com>
2026-02-16 14:32:00 +0300

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -12,9 +12,5 @@ class User
     public function __construct($id = null, $nom = null, $email = null, $type = null)
     {
         $this->db = Flight::db();
-        $this->id = $id;
-        $this->nom = $nom;
-        $this->email = $email;
-        $this->type = $type;
     }
 }
